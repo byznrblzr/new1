94c1a346b99b16bb255cf185395cf953eaba20e7
byznrblzr <beyzanurbulazar@gmail.com>
2024-11-05 09:15:38 +0300

    Add files via upload

---
+++ hello_python.py
@@ -0,0 +1,9 @@
+print("benim adım {}".format('beyza nur'))
+print ("benim adım {}, soyadım {}".format('beyza nur', 21))
+print("benim adım {}, soyadım {}, yaşım {}".format('beyza nur',21,20))
+print("benim adım {0}, soyadım {1}".format('beyza nur', 'bulazar'))
+print("benim adım {a}, soyadım {b}, yaşım{c}".format(a='beyza nur', b='bulazar', c=21))
+sayi=10 
+sayi=sayi+1 
+print(sayi)
+print('değişken yoksa tırnaksız parantez içini yazma')
